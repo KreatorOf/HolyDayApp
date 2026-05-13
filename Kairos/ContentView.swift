16d654a29b2054f36161770eb42aec26dc280aea
Matthias Cadet <matthias.cadet25@gmail.com>
2026-05-13 23:36:56 +0200

    Initial Commit

---
+++ Kairos/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Kairos
+//
+//  Created by Matthias Cadet on 13/05/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Kairos/KairosApp.swift
@@ -0,0 +1,17 @@
+//
+//  KairosApp.swift
+//  Kairos
+//
+//  Created by Matthias Cadet on 13/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct KairosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
